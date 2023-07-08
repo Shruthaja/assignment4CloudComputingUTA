d232cc50c5e089bbfe8378175873dda1f336b521
SHRUTHAJA <mail.shruthaja@gmail.com>
2023-07-07 22:04:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 server = 'assignmentservershruthaja.database.windows.net'
 database = 'assignemnt3'
 username = 'shruthaja'
-password = 'mattu4-12'
+password = ''
 driver = '{ODBC Driver 17 for SQL Server}'
 
 conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
